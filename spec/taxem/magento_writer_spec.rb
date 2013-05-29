25a0a2e482ca7330e5d222b87a1aa56892d2459b
Gabe Cook <gcook@aramisgroup.com>
2013-05-28 21:06:18 -0500

    fix magenta writer spec so it will run on hudson.

--- spec/taxem/magento_writer_spec.rb
+++ spec/taxem/magento_writer_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe Taxem::MagentoWriter do
   it "dumps the data" do
-   root_path = Pathname.new('/Users/gcook/code/taxem/spec/taxem/tax_data').realpath
    paths = {
-       path_to_boundaries: (root_path + 'NEB2013Q2FEB25.txt').realpath,
-       path_to_rates: (root_path + 'NER2013Q2FEB25.txt').realpath,
-       path_to_counties: (root_path + 'national.txt').realpath,
-       path_to_places: (root_path + 'PLACElist.txt').realpath
+       path_to_boundaries: boundary_data,
+       path_to_rates: rate_data,
+       path_to_counties: county_data,
+       path_to_places: place_data
    }
 
    writer = Taxem::MagentoWriter.new(paths)
